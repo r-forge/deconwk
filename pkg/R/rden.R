0ef7ff82164145617260a0b9af75b628b62fceb7
berwin (Berwin  Turlach) <Berwin.Turlach@gmail.com>
2010-07-15 09:28:59 +0000

    Added code for calculating density functions for the (contaminated) distribution used in the paper; help file shows how to produce Figure 1 of paper.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/deconwk@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rden.R
+++ pkg/R/rden.R
@@ -1,4 +1,4 @@
-rden <- function(N, DEN=1, sigma){
+rden <- function(N, DEN=1, sigma=0){
   switch(DEN,
          y <- rnorm(N,sd=sqrt(1+sigma^2)),
          y <- rnorm(N,sd=sqrt( sample(c(0.04,1), size=N,replace=T,prob=c(1,2)/3) + sigma^2)),
@@ -10,4 +10,14 @@ rden <- function(N, DEN=1, sigma){
          }
          )
   y
+}
+
+dden <- function(eval, DEN=1, sigma=0){
+  switch(DEN,
+         f <- dnorm(eval,mean=0,sd=sqrt(1+sigma^2)),
+         f <- 2*dnorm(eval,sd=sqrt(1+sigma^2))/3+dnorm(eval,sd=sqrt(0.04+sigma^2))/3,
+         f <- (dnorm(eval,mean=-2.5,sd=sqrt(1+sigma^2))+dnorm(eval,mean=2.5,sd=sqrt(1+sigma^2)))/2,
+         f <- 0.4*dnorm(eval,mean=5,sd=sqrt(5+sigma^2))+0.6*dnorm(eval,mean=13,sd=sqrt(13+sigma^2))
+         )
+  f
 }
